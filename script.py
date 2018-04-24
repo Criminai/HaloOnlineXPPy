9756597cb218c7b1850edba23a7d2c30ee8a843c
Puffycheeses <noahheague@gmail.com>
2018-04-24 17:46:04 +1000

    Fixed wrong xp > rank

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import math
 
 # All avaliable ranks
-rank = ['Recruit', 'Apprentice', 'Apprentice (Grade 2)', 'Private', 'Private (Grade 2)', 'Corpral', 'Copral (Grade 2)', 'Sergeant', 'Sergeant (Grade 2)', 'Sergeant (Grade 3)', 'Gunnery Sergeant', 'Gunnery Sergeant (Grade 2)', 'Gunnery Sergeant (Grade 3)', 'Gunnery Sergeant (Grade 4)', 'Lieutenant', 'Lieutentant (Grade 2)', 'Lieutenant (Grade 3)', 'Lieutenant (Grade 4)', 'Captain', 'Captain (Grade 2)', 'Captain (Grade 3)', 'Captain (Grade 4)', 'Major', 'Major (Grade 2)', 'Major (Grade 3)', 'Major (Grade 4)', 'Commander', 'Commander (Grade 2)', 'Commander (Grade 3)', 'Commander (Grade 4)', 'Colonel', 'Colonel (Grade 2)', 'Colonel (Grade 3)', 'Colonel (Grade 4)', 'Brigadier', 'Brigadier (Grade 2)', 'Brigadier (Grade 3)', 'Brigadier (Grade 4)', 'General', 'General (Grade 2)', 'General (Grade 3)', 'General (Grade 4) 5 Star General']
+rank = ['Apprentice', 'Apprentice (Grade 2)', 'Private', 'Private (Grade 2)', 'Corpral', 'Copral (Grade 2)', 'Sergeant', 'Sergeant (Grade 2)', 'Sergeant (Grade 3)', 'Gunnery Sergeant', 'Gunnery Sergeant (Grade 2)', 'Gunnery Sergeant (Grade 3)', 'Gunnery Sergeant (Grade 4)', 'Lieutenant', 'Lieutentant (Grade 2)', 'Lieutenant (Grade 3)', 'Lieutenant (Grade 4)', 'Captain', 'Captain (Grade 2)', 'Captain (Grade 3)', 'Captain (Grade 4)', 'Major', 'Major (Grade 2)', 'Major (Grade 3)', 'Major (Grade 4)', 'Commander', 'Commander (Grade 2)', 'Commander (Grade 3)', 'Commander (Grade 4)', 'Colonel', 'Colonel (Grade 2)', 'Colonel (Grade 3)', 'Colonel (Grade 4)', 'Brigadier', 'Brigadier (Grade 2)', 'Brigadier (Grade 3)', 'Brigadier (Grade 4)', 'General', 'General (Grade 2)', 'General (Grade 3)', 'General (Grade 4) 5 Star General']
 
 rankID = 0 
 xp = 0
